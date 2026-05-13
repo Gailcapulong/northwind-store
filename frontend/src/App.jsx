f634ef110ca1b98636d01359ca11e44e096b055f
Gailcapulong <gailcapulong77@gmail.com>
2026-05-13 22:15:15 +0800

    feat: implement product catalog and home page components; add image optimization utility and format functions

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,8 @@
 import { Show, SignInButton, SignUpButton, UserButton, useAuth } from '@clerk/react'
 import PageLoader from './components/PageLoader'
 import Layout from './components/Layout';
+import { Routes, Route } from 'react-router';
+import HomePage from './pages/HomePage';
 
 
 function App() {
@@ -8,27 +10,14 @@ function App() {
 
   if(!isLoaded) return <PageLoader />;
   
-
   return (
     <Layout>
-    <header>
-        <Show when="signed-out">
-          <SignInButton mode="modal"/>
-          <SignUpButton mode="modal" />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-
-      <p className="text-red-500">hello</p>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary">Click me</button>
-      <button className="btn btn-outline">Click me</button>
-
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
 
     </Layout>
-  )
+  );
 }
 
 export default App
